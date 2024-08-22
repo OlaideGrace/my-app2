0ea8a62646668144db03194854875c3f5aa5a8b2
OlaideGrace <olaide.awonusi@yahoo.com>
2024-08-22 10:45:47 +0100

    footer in process

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import styles from './styles.module.scss'
+import routes from '@routes'
+import Image from 'next/Image'
+
 
 export default function Footer() {
     return <>
-        <footer className={`w-100 d-flex ${styles.myLinks} `}>
+        <footer className={`bg-secondary w-100 d-flex ${styles.myLinks} `}>
+            <div>
+                <Link href={routes.home}>
+                <Image scr="/img/logo.png" alt="logo" width={120} height={45} />
+                </Link>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
             
         </footer>
     </>
