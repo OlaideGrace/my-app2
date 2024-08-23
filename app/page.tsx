61b0a63d93cf99da617603fee92b959d4044eb04
OlaideGrace <olaide.awonusi@yahoo.com>
2024-08-24 00:26:09 +0100

    my codes in progress

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,20 @@
 import { useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from 'swiper/react';
 import routes from "@/routes";
 import hero from "@/components/img/homepage-section11.png";
+import packages from "@/data/package";
+// import 'swiper/scss';
+// import 'swiper/scss/pagination';
+import { Autoplay, Pagination } from "swiper/modules"
 import styles from "@/app/page.module.scss";
 import Footer from "@/components/Footer/index";
 import Header from "@/components/Header/index";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+// import { SwiperContainer } from "@/components/SwiperContainer";
+
 
 export default function Home() {
 
@@ -31,9 +40,57 @@ export default function Home() {
         <h2 className="text-center">AMBASSADOR PACKAGES</h2>
         <p className="text-md-center col-md-9 col-lg-7">Our approach to teaching is unique and our teachers are extremely highly rated. We are affordable, simple, effective and very intensive </p>
         <div >
+          <Swiper 
+          autoHeight={true}
+          slidesPerView={1}
+          loop={true}
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 50,
+          },
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        className={styles.mySwiper}>
+          {
+            packages.map((item, index) => <SwiperSlide key={index}>
+              <div className="card px-2 py-">
+                <div className="card-body">
+                  <Image className="mb-3" src={item.thumbsnail} alt="img" width={50} height={50} />
+                  <h5 className="card-title">{item.title}</h5>
+                  <div className="card-text d-flex align-items-center gap-3 mb-4">
+                    {item.subtle}
+                  </div>
+                  <div>
+                    <Link href={item.link} className="text-decoration-none">
+                    <span className="text-secondary">Learn More</span>
+                    <FontAwesomeIcon className="ms-2" icon={ faArrowRight} /></Link>
+                  </div>
+                </div>
+              </div>
+            </swiperSlide>)
+          }
+          </swiper>
 
+          {/* <SwiperContainer/> */}
         </div>
-
       </section>
       </main>
 <Footer />
