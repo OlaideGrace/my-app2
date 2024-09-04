f9bd5180d4622e573b079e53890e1069c03aab3d
OlaideGrace <olaide.awonusi@yahoo.com>
2024-09-04 14:54:44 +0100

    4-9

--- components/AmbassadorContainer/index.tsx
+++ components/AmbassadorContainer/index.tsx
@@ -7,19 +7,21 @@ export default  function AmbassadorContainer() {
     return (
         <>
             <div className={styles.ambassadorContainer}>
-                {
-                    packages.map((data)=><div className={`${styles.row} row d-flex`} key={data.title}>
-                            <div className={`$col-xs-12 col-md-6 d-flex justify-content-center p-3 order-0 order-md-${data.order}`}>
-                                <Image src={data.img} alt={`${data.title} image`} className={`${styles.image} w-100`}/>
+                {packages.map((data)=><div className={`${styles.row} row d-flex`} key={data.title}>
+                            <div className={`col-xs-12 col-md-6 d-flex justify-content-center p-3 order-0 order-1 0rder-0 order-md-${data.order}`}>
+                                <div className={styles.imageContainer}>
+                                <Image src={data.img} alt={`${data.title} image`} className={styles.image} />
+                                </div>
                             </div>
                             <div className="col-xs-12 col-md-6">
-                                <div>
+                                <div className={styles.content}>
                                     <p>Easier desion making for</p>
-                                    <h1>{data.title} AMBASSADOR</h1>
-                                    <div>
+                                    <h3>{data.title} AMBASSADOR</h3>
+                                    <div className={styles.descriptions}>
                                         {data.descriptions.map(
                                             (dataDescription)=><div className="d-flex" key={dataDescription}>
-                                                <Image className="md-5" src={"/checkList.svg"} alt={"checklist"} width={20} height={20}/>
+                                                <Image src={"/checkList.svg"} alt={"checklist"} width={20} height={20}
+                                                className={styles.checklistImage} />
                                                 <p>{dataDescription}</p>
                                             </div>
                                         )}
